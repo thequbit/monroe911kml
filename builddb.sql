f1ef23b5cc192688a3e457e807ab99440d1e77ac
thequbit <thequbit@gmail.com>
2012-12-25 03:46:18 -0500

    updated build sql script with permisions commands

--- builddb.sql
+++ builddb.sql
@@ -1,5 +1,9 @@
 create database monroe911;
 
+grant usage on _database_.* to _username_ identified by _passowrd_;
+
+grant all privileges on _database_.* to _username_ ;
+
 create table tweets ( tweetid int not null auto_increment primary key, tweettext text not null, lat int not null, lng int not null, tweetdate date not null, tweettime time not null);
 
 
